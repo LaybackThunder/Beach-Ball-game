6b810c1c0d85fb0e7237dcaf3b3e2fb34ecc5f38
LaybackThunder <glowingblueasteroid@outlook.com>
2022-12-28 11:02:30 -0900

    Add settings to repository.

---
+++ settings.py
@@ -0,0 +1,10 @@
+class Settings():
+    """A class to store all settings for Catch The Beach Ball."""
+
+    def __init__(self):
+        """Initialize the games settings."""
+        # Screen settings
+        self.SCREEN_WIDTH = 1200
+        self.SCREEN_HEIGHT = 800
+        self.LIGHT_GRAY = (230, 230, 230)
+        self.BACKGROUND_COLOR = self.LIGHT_GRAY
