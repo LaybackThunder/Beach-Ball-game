ce107842d8ced44f3b692ea935161f8f41162d9b
LaybackThunder <glowingblueasteroid@outlook.com>
2022-12-28 10:33:57 -0900

    Add empty Pygame window.

---
+++ client.py
@@ -0,0 +1,30 @@
+import pygame, sys
+
+class Client():
+    """Overall class to manage game assets and behavior."""
+
+    def __init__(self):
+        """Initialize the game, and create game resources."""
+        pygame.init()
+
+        self.screen = pygame.display.set_mode(1200, 800)
+        pygame.display.set_caption('Beach Ball')
+
+    def run_game(self):
+        """Start the main loop for the game."""
+
+        while True:
+            # Watch for keyboard and mouse events.
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+            
+            # Make most recently drawn screen visible
+            pygame.display.update()
+
+
+
+if __name__ == '__main__':
+    # Make game instance and run the game.
+    client = Client()
+    client.run_game()
