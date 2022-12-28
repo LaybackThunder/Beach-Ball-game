6d823997f05811e63b8ddf8abf8105352d86fed4
LaybackThunder <glowingblueasteroid@outlook.com>
2022-12-28 10:59:33 -0900

    Add background color and create settings class, link it to client.

--- client.py
+++ client.py
@@ -1,14 +1,22 @@
 import pygame, sys
 
+from settings import Settings
+
 class Client():
     """Overall class to manage game assets and behavior."""
 
     def __init__(self):
         """Initialize the game, and create game resources."""
         pygame.init()
+        self.settings = Settings()
 
-        self.screen = pygame.display.set_mode(1200, 800)
+        # Pygame screen
+        self.screen = pygame.display.set_mode(
+            (self.settings.SCREEN_WIDTH, self.settings.SCREEN_HEIGHT))
         pygame.display.set_caption('Beach Ball')
+
+        self.LIGHT_GRAY = (230, 230, 230)
+        self.BACKGROUND_COLOR = (self.LIGHT_GRAY)
 
     def run_game(self):
         """Start the main loop for the game."""
@@ -18,6 +26,9 @@ class Client():
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+
+            # Light gray background
+            self.screen.fill(self.settings.BACKGROUND_COLOR)
             
             # Make most recently drawn screen visible
             pygame.display.update()
